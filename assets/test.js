95294017177a7b01b224e740466c736847e71bd1
chenchen <wb-chenchen.c@alibaba-inc.com>
2013-11-04 14:23:12 +0800

    change js

--- assets/test.js
+++ assets/test.js
@@ -1,3 +1,11 @@
-(function(){
-  console.log('this is a remote test form github');
-})();
+(function(S){
+	var D = S.DOM,
+		E = S.Event;
+
+	D.insertBefore(D.create('<li class="sel-all" style="cursor:pointer;" id="J_sel_all"><a>ȫѡ</a></li>'), S.one('.nav-01'));
+	E.on(S.one("#J_sel_all"), "click", function(e){
+		e.preventDefault();
+		S.all(".checkbox input").attr("checked", true);
+	});
+    
+})(KISSY);
