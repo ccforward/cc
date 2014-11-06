37ae06c101bf1c8208077bf3b5a6c7791343797b
chenchen <wb-chenchen.c@alibaba-inc.com>
2014-11-06 20:15:41 +0800

    add blog

--- chrome-extension/startup/assets/content.js
+++
@@ -1,9 +0,0 @@
-chrome.extension.sendMessage({ 
-	links: sessionStorage['init-file-links'].split(','), 
-	scripts: sessionStorage['init-file-scripts'].split(','), 
-	location: document.location, 
-	localStg: {
-		'request':localStorage["send_head_request"],
-		'files':localStorage["monitor_files"]
-	}
-});
