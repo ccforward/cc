052f71afa73eb9b2275c870120c9b7dbc053cc41
chenchen <wb-chenchen.c@alibaba-inc.com>
2014-12-05 15:37:45 +0800

    add rgb to hex

---
+++ RGB2HEX/index.js
@@ -0,0 +1,15 @@
+var RGB2Hex = function(color) {
+    if (color.substr(0, 1) === '#') {
+        return color;
+    }
+    var digits = /(.*?)rgb\((\d+), (\d+), (\d+)\)/.exec(color);
+
+    var red = parseInt(digits[2]);
+    var green = parseInt(digits[3]);
+    var blue = parseInt(digits[4]);
+
+    var rgb = blue | (green << 8) | (red << 16);
+    return digits[1] + '#' + rgb.toString(16);
+}
+
+console.log(RGB2Hex('rgb(23, 227, 193)'));
