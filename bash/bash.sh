eb467c7bb8880d7af10dd57ba3c45daa21b4a96a
ccforward <ccking@foxmail.com>
2014-12-23 09:38:23 +0800

    update bash

--- bash/bash.sh
+++ bash/bash.sh
@@ -1,7 +1,19 @@
+#grep
+alias grep='grep --color=always'
+alias ls='ls -G'
+alias ll='ls -l'
+alias vi='mvim'
+
+# show hide files
+alias show='defaults write com.apple.finder AppleShowAllFiles -bool true'
+alias hide='defaults write com.apple.finder AppleShowAllFiles -bool false'
+
 #Apache的根目录
 alias fk='cd /Library/WebServer/Documents/'
+
 # 工具启动
 alias fire='open /Applications/Sublime\ Text\ 2.app/ & open /Applications/Google\ Chrome.app'
+
 #mysql
 alias mysql=/usr/local/mysql/bin/mysql
 alias mysqladmin=/usr/local/mysql/bin/mysqladmin
