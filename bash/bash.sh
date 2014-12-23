5aa7bb8db313b0c8a2cfb054b4d9b0bb03f99b61
ccforward <ccking@foxmail.com>
2014-12-23 09:34:56 +0800

    add bash

---
+++ bash/bash.sh
@@ -0,0 +1,9 @@
+#Apache的根目录
+alias fk='cd /Library/WebServer/Documents/'
+# 工具启动
+alias fire='open /Applications/Sublime\ Text\ 2.app/ & open /Applications/Google\ Chrome.app'
+#mysql
+alias mysql=/usr/local/mysql/bin/mysql
+alias mysqladmin=/usr/local/mysql/bin/mysqladmin
+
+#git
