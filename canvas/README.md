02d02d82513a8159743e948d11bc2460e4dbc9b6
ccforward <ccking@foxmail.com>
2014-12-25 13:37:00 +0800

    delete something

---
+++ canvas/README.md
@@ -0,0 +1,5 @@
+#canvas 笔记
+
+## demo
+
+
--- chrome-extension/fresh/background.js
+++ chrome-extension/fresh/background.js
@@ -1,6 +1,6 @@
 var buttonClicked = function() {
 
-    // var scriptSource = 'http://cc.etao.com/fresh/assets/refresh.js',
+    // var scriptSource = 'http://127.0.0.1/fresh/assets/refresh.js',
     //     script = [
     //         'var head = document.getElementsByTagName("head")[0];',
     //         'var script = document.createElement("script");',
