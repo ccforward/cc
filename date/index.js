e0bcff0a529317bd15c5d2e21ca5f64cef9ab904
ccforward <ccking@foxmail.com>
2015-04-06 09:58:05 +0800

    fixbug

--- date/index.js
+++ date/index.js
@@ -3,17 +3,15 @@ function DateCalc(date,bef,aft){
     this.date = date.substr(0,4) + '-' + date.substr(4,2) + '-' + date.substr(-2);
     this.bef = bef || 0;
     this.aft = aft || 0;
-    this.now = now;
-
-    function now() {
-        var d = new Date();
-        return [d.getFullYear(), this._cover(d.getMonth()+1), this._cover(d.getDate())].join('');     
-    }
 }
 
 DateCalc.prototype = {
     constructor: DateCalc,
     
+    now: function(){
+        var d = new Date();
+        return [d.getFullYear(), this._cover(d.getMonth()+1), this._cover(d.getDate())].join('');     
+    },
     before: function(days){
         return this._calc(days,'before');
     },
@@ -42,6 +40,4 @@ DateCalc.prototype = {
         return n < 10 ? '0' + n : n;
     }
 }
-
-module.exports = DateCalc;
 
