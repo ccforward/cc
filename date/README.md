12da9a3a84013cef285dc001a396c54f6e3673ba
cc <ccking@foxmail.com>
2016-10-06 16:48:13 +0800

    update date calc tool date.js

--- date/README.md
+++ date/README.md
@@ -1,30 +1,53 @@
-# 简单计算某一天前后日期的类 DateCalc
+# Simple Date Calculator: DateCalc
 
-## 用法
+A simple date calculator from my personal project [Zhihu-Spider](https://github.com/ccforward/zhihu)
 
-``` javascript
-var d = new DateCalc('20150401','1','30');
-console.log(d.before());
-console.log(d.after());
-console.log(d.before(2));
-console.log(d.after(2));
-console.log(d.now());
+## Usage
+
+``` js
+const DateCalc = require('date-calc')
+// or ES2015
+// import DateCalc from 'date-calc'
+
+let d = new DateCalc('20460818');
+d.before();
+d.after();
+d.before(2);
+d.after(2);
+d.now();
+d.month();
+d.beforeMonth()
+d.afterMonth()
 
 ```
 
-* 参数
-    * 20150401 基准日期
-    * 1 往前计算的天数
-    * 30 往后计算的天数
-* 方法
-    * d.before() 往前计算后的日期
-    * d.after() 往后计算的日期
-    * d.before(2) 基准日期前的2天 忽略初始天数
-    * d.after(2) 基准日期后2的天 忽略初始天数
-    * d.now() 当前日期
-* 其他
-    * new DateCalc(),无参数实例化时 before() after() 返回当前日期
+## Parameters
 
-## TODO
-* 输入日期模板化  {YYYYMMDD} {YY-MM-DD}
-* 精确到时分秒 默认00:00:00
+* `new DateCalc()` no parameters means today
+* `new DateCalc('20460818')`  reference date
+
+## Instance Method
+
+* `d.before()` the day before reference date 20460817
+* `d.after()` the day after reference date 20460819
+* `d.before(2)` twos days before reference date 忽略初始天数 20460816
+* `d.after(2)` twos days after reference date 20460820
+* `d.now()` now date 20460818
+* `d.month()` the month of reference date 204608
+* `d.beforeMonth()` the month before reference date 204607
+* `d.afterMonth()` the month after reference date 204609
+* month English descripe
+  * `new DateCalc('20460118').monthEN()` returns 'Jan'
+  * `new DateCalc('20460218').monthEN()` returns 'Feb'
+  * `new DateCalc('20460318').monthEN()` returns 'Mar'
+  * `new DateCalc('20460418').monthEN()` returns 'Apr'
+  * `new DateCalc('20460518').monthEN()` returns 'May'
+  * `new DateCalc('20460618').monthEN()` returns 'Jun'
+  * `new DateCalc('20460718').monthEN()` returns 'Jul'
+  * `new DateCalc('20460818').monthEN()` returns 'Aug'
+  * `new DateCalc('20460918').monthEN()` returns 'Sep'
+  * `new DateCalc('20461018').monthEN()` returns 'Oct'
+  * `new DateCalc('20461118').monthEN()` returns 'Nov'
+  * `new DateCalc('20461218').monthEN()` returns 'Dec'
+* month Chinese descripe  
+  `new DateCalc('20460118').CHN()` returns '2046年01月18日'
--- date/index.js
+++ date/index.js
@@ -1,50 +1,91 @@
 // 日期计算 
-// 格式 date: 20150401
-function DateCalc(date,bef,aft){
+// 格式 date: 20460101
+function DateCalc(date, bef, aft){
     if(date){
         this.date = [date.substr(0,4), '-', date.substr(4,2), '-', date.substr(-2)].join('')
     }else{
         var d = new Date(); 
         this.date = [d.getFullYear(), '-', d.getMonth()+1, '-', d.getDate()].join('')
     }
-    // this.date = date ? [date.substr(0,4), '-', date.substr(4,2), '-', date.substr(-2)].join('') : (var d = new Date(); return [d.getFullYear(), '-', d.getMonth()+1, '-', d.getDate()].join(''));
     this.bef = bef || 0;
     this.aft = aft || 0;
+    this.monthArr = ['00','01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12'];
+    this.monthENArr = ['','Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
 }
 
 DateCalc.prototype = {
     constructor: DateCalc,
-    
+
     now: function(){
-        var d = new Date();
+        var d = this.date ? new Date(this.date) : new Date();
         return [d.getFullYear(), this._cover(d.getMonth()+1), this._cover(d.getDate())].join('');     
     },
     before: function(days){
-        return days ? this._calc(days,'before') : this.now();
+        return this._calc(days || 1,'before');
     },
     after: function(days){
-        return days ? this._calc(days,'after') : this.now();
+        return this._calc(days || 1,'after');
+    },
+    month: function(){
+        var d = this.date ? new Date(this.date) : new Date();
+        return [d.getFullYear(), this._cover(d.getMonth()+1)].join('');     
+    },
+    monthEN: function(){
+        var d = this.date ? new Date(this.date) : new Date();
+        return this.monthENArr[parseInt(this.month().substr(4,2))];    
+    },
+    beforeMonth: function(){
+        var y = parseInt(this.month().substr(0,4), 10),
+            m = this.month().substr(4,2),
+            idx = this.monthArr.indexOf(m);
+        if(idx == 1){
+            m = '12';
+            y--;
+        }else {
+            m = this.monthArr[idx-1];
+        }
+        return y + '' + m;
+    },
+    afterMonth: function(){
+        var y = parseInt(this.month().substr(0,4), 10),
+            m = this.month().substr(4,2),
+            idx = this.monthArr.indexOf(m);
+        if(idx == 12){
+            m = '01';
+            y++;
+        }else {
+            m = this.monthArr[idx+1];
+        }
+        return y + '' + m;
+    },
+    CHN: function(dtime){
+        dtime = dtime ? dtime : this.now()
+        var y = dtime.substr(0,4)+'年',
+            m = dtime.substr(4,2)+'月',
+            d = dtime.substr(6,2)+'日';
+        return y+m+d;
     },
     // days 前后的天数
     // type before 或 after
     _calc: function(days, type){
-        var _self = this,
-            d = new Date(_self.date),
+        var d = new Date(this.date),
             input = 0;
             if(type === 'before') {
-                input = 0 - _self.bef;
+                input = 0 - this.bef;
                 days = 0 - days;
             }else {
-                input = _self.aft;
+                input = this.aft;
             }
             var total = days || input || 0;
 
         var newDate = new Date(d.getTime() + 3600*24*1000*total);
-        return [newDate.getFullYear(), _self._cover(newDate.getMonth()+1), _self._cover(newDate.getDate())].join('');
+        return [newDate.getFullYear(), this._cover(newDate.getMonth()+1), this._cover(newDate.getDate())].join('');
     },    
     _cover:function(num){
         var n = parseInt(num, 10);
         return n < 10 ? '0' + n : n;
     }
 }
+
+module.exports = DateCalc;
 
