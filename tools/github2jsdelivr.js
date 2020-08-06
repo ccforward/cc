8c5f030563eae2b1ec5c44b35133737d08848e21
cc <ccking@foxmail.com>
2020-08-06 16:34:26 +0800

    feat: add chrome script

--- tools/github2jsdelivr.js
+++ tools/github2jsdelivr.js
@@ -13,3 +13,7 @@ if (['jpg', 'gif', 'png', 'svg'].includes(path.split('.')[1])) {
   var img = document.querySelector('img[src="' + path + '?raw=true"]');
   img.parentNode.insertBefore(a, img)
 }
+
+// chrome 书签
+
+javascript:var path = location.pathname;var cdn='https://cdn.jsdelivr.net/gh'+path.replace('/blob/','@');var a=document.querySelector('#raw-url');a.href=cdn;a.setAttribute('target','_blank');if(['jpg','gif','png','svg'].includes(path.split('.')[1])){var a=document.createElement('a');a.setAttribute('style','display:block;font-size:16px');a.target="_blank";a.href=cdn;a.innerHTML=cdn;var img = document.querySelector('img[src="'+path+'?raw=true"]');img.parentNode.insertBefore(a, img)}
